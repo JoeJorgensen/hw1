18891676ed0e17f29e2578911b64fa1e1771ea07
JoeJorgensen <joebjorg@gmail.com>
2022-02-15 12:17:48 -0700

    Function Practice

--- function.js
+++ function.js
@@ -23,3 +23,27 @@ function introducePerson(person){
 introducePerson('Joe')
 introducePerson('John')
 introducePerson('Jill')
+
+function add(num1 , num2) {
+    // console.log('num1', num1)
+    // console.log(num1 + num2)
+return num1 + num2
+}
+console.log(typeof add(1,1))
+console.log(typeof 2 === 2)
+console.log(typeof 2 === 3)
+console.log(add ('1','1'))
+let sum = add(1,1)
+console.log(sum)
+let sum1 = add(add(2,2),add(3,3))
+console.log(sum1)
+
+const evenOrOdd = (num)=>{
+    if(num % 2===0){
+        return'even'
+    } else {
+        return 'odd'
+    }
+}
+let x = evenOrOdd(3)
+console.log(x)
